5716c326f7e1d3d017c7cf10bf6b5d5cfab3ff04
sin0713 <narisin1124@gmail.com>
2024-04-08 23:01:18 +0900

    abc276 C

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,28 @@
-import abc.abc276.Abc276B
-import abc.abc277.Abc277D
 
+class Abc276C {
+    fun solve() {
+        val n = readln().toInt()
+        val p = readln().split(" ").map { it.toInt() }.toIntArray()
+
+        for (i in n-2 downTo 0) {
+            if (p[i] > p[i+1]) {
+                var j = n-1
+                while(p[i] < p[j]) j--
+                val temp: Int = p[i]
+                p[i] = p[j]
+                p[j] = temp
+                p.reverse(i+1, n)
+                break
+            }
+        }
+        repeat(n) {
+            print("${p[it]} ")
+        }
+        println()
+    }
+}
 fun main() {
-    Abc276B().solve()
+    Abc276C().solve()
 }
 
 
---
+++ src/main/kotlin/abc/abc276/Abc276C.kt
@@ -0,0 +1,24 @@
+package abc.abc276
+
+class Abc276C {
+    fun solve() {
+        val n = readln().toInt()
+        val p = readln().split(" ").map { it.toInt() }.toIntArray()
+
+        for (i in n-2 downTo 0) {
+            if (p[i] > p[i+1]) {
+                var j = n-1
+                while(p[i] < p[j]) j--
+                val temp: Int = p[i]
+                p[i] = p[j]
+                p[j] = temp
+                p.reverse(i+1, n)
+                break
+            }
+        }
+        repeat(n) {
+            print("${p[it]} ")
+        }
+        println()
+    }
+}
