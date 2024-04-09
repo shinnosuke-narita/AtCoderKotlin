a9de2e1543833c58db230bad53e8ccc9ce2ca8bd
sin0713 <narisin1124@gmail.com>
2024-04-10 08:03:38 +0900

    abc274C

--- src/main/kotlin/abc/abc274/Abc274C.kt
+++ src/main/kotlin/abc/abc274/Abc274C.kt
@@ -3,16 +3,6 @@ package abc.abc274
 import java.util.*
 
 class Abc274C {
-    private val memo = mutableMapOf<Int, Int>()
-
-    private fun f(x: Int): Int {
-        if (x == 1) return 0
-
-        if (memo.containsKey(x)) return memo[x]!!
-
-        memo[x] = f(x/2)+1
-        return memo[x]!!
-    }
 
     fun solve() {
         val n: Int
@@ -21,15 +11,19 @@ class Abc274C {
             n = sc.nextInt()
             a = IntArray(n)
             repeat(n) { i -> a[i] = sc.nextInt() }
-        }
 
-        println("${f(1)}")
-        repeat(n) { i ->
-            val a1 = a[i] * 2
-            val a2 = a1 + 1
+            val m = 2*n+2
+            val ans = IntArray(m) {0}
+            for (i in 1..n) {
+                val parent = a[i-1]
+                ans[i*2] = ans[parent]+1
+                ans[i*2+1] = ans[parent]+1
+            }
 
-            println("${f(a1)}")
-            println("${f(a2)}")
+            repeat(m) {i ->
+                if (i == 0) return@repeat
+                println("${ans[i]}")
+            }
         }
     }
 }
